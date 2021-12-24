4f4525f0c02e4414ecf3b6c60787c08201002498
shwetakokane <95267688+shwetakokane@users.noreply.github.com>
2021-12-24 12:56:52 +0530

    Create server.js

---
+++ Assignment07/js/server.js
@@ -0,0 +1,28 @@
+function myLogger(){
+
+             let occasion = document.getElementById("occasion").value;
+
+             switch(occasion){
+        case "Birthday":
+        console.log("Happy Birthday");
+        break;
+
+        case "Anniversary": 
+        console.log("Happy Anniversary");
+        break;
+
+        case "Christmas": 
+        console.log("Merry Christmas");
+        break;
+
+        case "New year": 
+        console.log("Happy New Year");
+        break;
+
+    }
+}
+   
+
+
+
+
